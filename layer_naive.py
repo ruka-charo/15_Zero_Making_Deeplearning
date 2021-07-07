633eb36b4288cd000423940987650d912997a265
ruka charo <ruka.and.little.charo1@gmail.com>
2021-07-08 07:54:46 +0900

    Study_day4

--- layer_naive.py
+++ layer_naive.py
@@ -1,3 +1,10 @@
+import sys, os
+sys.path.append(os.pardir)
+os.chdir('/Users/rukaoide/Library/Mobile Documents/com~apple~CloudDocs/Documents/Python/15_Zero_Making_Deeplearning')
+from common.functions import softmax, cross_entropy_error
+import numpy as np
+
+
 class MulLayer: # 乗算レイヤー
     def __init__(self):
         self.x = None
@@ -46,4 +53,55 @@ class Relu:
     def backward(self, dout):
         dout[self.mask] = 0
         dx = dout
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1 - self.out) * self.out
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = x @ self.W + self.b
+        return out
+
+    def backward(self, dout):
+        dx = dout @ self.W.T
+        self.dW = self.x.T @ dout
+        self.db = np.sum(dout, axis=0)
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 損失
+        self.y = None # softmaxの出力
+        self.t = None # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
         return dx
